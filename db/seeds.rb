31cd10dc876499d7784dad1e1e62768eeefa836d
osha7 <ogroetz@gmail.com>
2021-04-28 20:54:17 -0600

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Musician.all.destroy
+Song.all.destroy
+
+aretha = Musician.create(name: "Aretha Franklin", origin: "Memphis, Tennessee", is_active: false)
+nina = Musician.create(name: "Nina Simone", origin: "Tryon, North Carolina", is_active: false)
+ella = Musician.create(name: "Ella Fitzgerald", origin: "Newport News, Virgina", is_active: false)
+
+respect = Song.create(name: "Respect", musician_id: 1)
+sinnerman = Song.create(name: "Sinnerman", musician_id: 2)
+it_dont_mean_a_thing = Song.create(name: "It Don't Mean A Thing", musician_id: 3)
